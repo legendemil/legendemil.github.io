283e1789f60b283ba0268cc22cb06dfc33421b2b
Emil Pausz <emilpausz@gmail.com>
2016-08-25 15:26:39 +0200

    init commit

---
+++ sites/todo_app/js/todosDB.js
@@ -0,0 +1,47 @@
+let PouchDB = require('pouchdb'), 
+	todosDB = todosDB || { },
+	pubSub = require('pubsub-js');
+
+PouchDB.plugin(require('pouchdb-upsert'));
+todosDB.db =  new PouchDB('todos');
+
+todosDB.addTodo = function(todo) {
+	return this.db.put({
+			_id: todo._id,
+			title: todo.title,
+			task: todo.task,
+			priority: todo.priority,
+			is_done: todo.is_done
+		}).then( (res) => {
+			todo._rev = res.rev;
+			pubSub.publish('ADD_TODO', todo);
+		});
+}
+
+todosDB.update = function(todo) {
+		return this.db.upsert(todo._id, function(doc){
+			for(let prop in todo) {
+				doc[prop] = todo[prop];
+			}
+			return doc;
+		}).then( res => console.log('updated', res) );
+}
+
+todosDB.removeTodo = function(_id, _rev) {
+	console.log('removing', _id, _rev);
+	return this.db.remove(_id, _rev);
+}
+
+todosDB.getSingleTodo = function(_id) {
+	return this.db.get(_id);
+}
+
+todosDB.getAll = function() {
+	return this.db.allDocs({
+		include_docs: true,
+		descending: true
+	});
+}
+
+
+module.exports = todosDB;
